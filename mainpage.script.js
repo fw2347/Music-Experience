7b5d076e7dfcde6a60906405f28dc62968848d9a
fw2347 <65837204+fw2347@users.noreply.github.com>
2020-08-13 17:34:15 -0400

    Update mainpage.script.js

--- mainpage.script.js
+++ mainpage.script.js
@@ -4,8 +4,4 @@ ityped.init('#ityped', {
     loop: true
   });
 
-	<link rel="stylesheet" href="mainpage.style.css">
-	<link href="https://fonts.googleapis.com/css2?family=Antic+Slab&display=swap" rel="stylesheet">
-	<link href="https://code.jquery.com/jquery-3.5.1.slim.min.j"
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js">
-	<link href="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js">
+
