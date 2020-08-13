cd007aeb5cc940c69c1ba2ed1725e7c08321806c
fw2347 <65837204+fw2347@users.noreply.github.com>
2020-08-13 14:19:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ ityped.init('#ityped', {
     startDelay: 200,
     loop: true
   });
+
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Antic+Slab&display=swap" rel="stylesheet">
+	<link href="https://code.jquery.com/jquery-3.5.1.slim.min.j"
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js">
+	<link href="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js">
